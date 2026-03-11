904a37dcd2db895e0b21fc59c5996c58bec364f7
cenxincan <1922708711@qq.com>
2026-03-11 16:01:00 +0800

    yhua

--- src/api/server.js
+++ src/api/server.js
@@ -17,7 +17,7 @@ export const getStationAntennaList = (stationId, params) => {
   const isTest =
     location.search.indexOf("isTest") >= 0 &&
     location.search.split("isTest=")[1];
-  if (isTest === "true") {
+  if (true) {
     return mockServer[1].response().data;
   } else {
     return request.post(`${baseUrl}/3d/station/${stationId}`, {
